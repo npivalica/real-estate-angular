7ad185a691cfa1c43b17dac090dae5dea7f4df2a
[Nikolina Pivalica] <[nikolina.pivalica@gmail.com]>
2021-09-17 22:39:33 +0200

    Fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './pages/about/about.component';
+import { AgentsGridComponent } from './pages/agents-grid/agents-grid.component';
+import { AgentSingleComponent } from './pages/agent-single/agent-single.component';
+import { BlogGridComponent } from './pages/blog-grid/blog-grid.component';
+import { BlogSingleComponent } from './pages/blog-single/blog-single.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PropertyGridComponent } from './pages/property-grid/property-grid.component';
+import { PropertySingleComponent } from './pages/property-single/property-single.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'agent-single', component: AgentSingleComponent },
+  { path: 'agents-grid', component: AgentsGridComponent },
+  { path: 'blog-grid', component: BlogGridComponent },
+  { path: 'blog-single', component: BlogSingleComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'property-grid', component: PropertyGridComponent },
+  { path: 'property-single', component: PropertySingleComponent },
+  { path: '', component: HomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,15 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { AboutComponent } from './pages/about/about.component';
-import { AgentsGridComponent } from './pages/agents-grid/agents-grid.component';
-import { AgentSingleComponent } from './pages/agent-single/agent-single.component';
-import { BlogGridComponent } from './pages/blog-grid/blog-grid.component';
-import { BlogSingleComponent } from './pages/blog-single/blog-single.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { PropertyGridComponent } from './pages/property-grid/property-grid.component';
-import { PropertySingleComponent } from './pages/property-single/property-single.component';
+// import { AboutComponent } from './pages/about/about.component';
+// import { AgentsGridComponent } from './pages/agents-grid/agents-grid.component';
+// import { AgentSingleComponent } from './pages/agent-single/agent-single.component';
+// import { BlogGridComponent } from './pages/blog-grid/blog-grid.component';
+// import { BlogSingleComponent } from './pages/blog-single/blog-single.component';
+// import { ContactComponent } from './pages/contact/contact.component';
+// import { HomePageComponent } from './pages/home-page/home-page.component';
+// import { PropertyGridComponent } from './pages/property-grid/property-grid.component';
+// import { PropertySingleComponent } from './pages/property-single/property-single.component';
 import { MenuComponent } from './layout/menu/menu.component';
 
 // const appRoutes : Routes = [
@@ -35,20 +35,34 @@ import { MenuComponent } from './layout/menu/menu.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AboutComponent,
-    AgentsGridComponent,
-    AgentSingleComponent,
-    BlogGridComponent,
-    BlogSingleComponent,
-    ContactComponent,
-    HomePageComponent,
-    PropertyGridComponent,
-    PropertySingleComponent,
+    // AboutComponent,
+    // AgentsGridComponent,
+    // AgentSingleComponent,
+    // BlogGridComponent,
+    // BlogSingleComponent,
+    // ContactComponent,
+    // HomePageComponent,
+    // PropertyGridComponent,
+    // PropertySingleComponent,
     MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    // AboutComponent,
+    // AgentsGridComponent,
+    // AgentSingleComponent,
+    // BlogGridComponent,
+    // BlogSingleComponent,
+    // ContactComponent,
+    // HomePageComponent,
+    // PropertyGridComponent,
+    // PropertySingleComponent,
+    MenuComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
